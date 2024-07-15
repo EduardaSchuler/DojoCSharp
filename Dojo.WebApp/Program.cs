47d47bfb63c3f2436777be68211d323c6d9fff63
eduardaschuler <dudaschuler2011@gmail.com>
2024-07-15 16:07:59 -0300

    adiciona dependencias

--- Dojo.WebApp/Program.cs
+++ Dojo.WebApp/Program.cs
@@ -1,6 +1,21 @@
+using Dojo.DAO.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DbdojoContext>((serviceProvider, options) =>
+{
+	var isTest = Environment.GetEnvironmentVariable("IS_TEST");
+	if (isTest == "True")
+	{
+		options.UseInMemoryDatabase("TestDatabase");
+	}
+	else
+	{
+		options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+	}
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
